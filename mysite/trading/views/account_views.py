e4d1996c87855f44f0db0c70a9e7975cbf7db1d2
JBaker98 <34064544+JBakes98@users.noreply.github.com>
2020-01-20 20:53:38 +0000

    split up view file

---
+++ mysite/trading/views/account_views.py
@@ -0,0 +1,181 @@
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views.generic import TemplateView
+
+from mysite.trading.backend import user_bookmarks, user_positions
+from mysite.trading.forms import SignUpForm, EditAccountForm
+
+
+class AccountView(TemplateView):
+    model = User
+    template_name = 'account/account.html'
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if not request.user.is_authenticated:
+            return redirect('login')
+
+        return super(AccountView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountView, self).get_context_data(**kwargs)
+        request = self.request
+        user = request.user
+
+        if request.user.is_authenticated:
+
+            user_bookmarks.get_user_favourites(user, context)
+            user_positions.get_open_positions(user, context)
+            user_positions.get_closed_positions(user, context)
+
+            for fav in context['favourites'].iterator():
+                user_bookmarks.check_is_favourite(user, fav, context)
+
+            return context
+
+        else:
+            messages.success(request, "Please log in")
+            return context
+
+
+def signup(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = True
+                user.save()
+
+                current_site = get_current_site(request)
+                subject = 'Activate Your Trading Account'
+                message = render_to_string('account/account_activation_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    #'token': account_activation_token.make_token(user),
+                })
+                user.email_user(subject, message)
+                return redirect('index')
+
+            else:
+                for msg in form.error_messages:
+                    messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
+                    return render(request,
+                                  "account/signup.html",
+                                  {"form": form})
+
+        else:
+            form = SignUpForm()
+        return render(request, 'account/signup.html', {'form': form})
+
+    else:
+        return redirect('index')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None: #and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.account.email_confirmed = True
+        user.save()
+        login(request, user)
+        messages.success(request, f"New account activated: {user.email}")
+        return redirect('index')
+    else:
+        return render(request, 'account/account_activation_invalid.html')
+
+
+def account_activation_sent(request):
+    return render(request, 'account/account_activation_sent.html')
+
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request,
+                  'account/login.html',
+                  {'form': form})
+
+
+def logout_request(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.info(request, 'Logged Out')
+
+        return redirect('index')
+    else:
+        return redirect('index')
+
+
+def edit_account(request):
+    if request.user.is_authenticated:
+
+        if request.method == 'POST':
+            form = EditAccountForm(request.POST, instance=request.user)
+
+            if form.is_valid():
+                form.save()
+                return redirect('/account')
+
+        else:
+            form = EditAccountForm(instance=request.user)
+            args = {'form': form}
+            return render(request,
+                          'account/edit_account.html',
+                          args)
+    else:
+        return redirect('login')
+
+
+def change_password(request):
+    if request.user.is_authenticated:
+
+        if request.method == 'POST':
+            form = PasswordChangeForm(data=request.POST, user=request.user)
+
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(request, form.user)
+                return redirect('/account')
+            else:
+                return redirect('/account/change_password')
+
+        else:
+            form = PasswordChangeForm(user=request.user)
+            args = {'form': form}
+            return render(request,
+                          'account/change_password.html',
+                          args)
+    else:
+        return redirect('login')
+
+
+
---
+++ mysite/trading/views/exchange_views.py
@@ -0,0 +1,53 @@
+from django.views import generic
+
+from mysite.trading.backend import user_bookmarks, user_positions
+from mysite.trading.models import Stock, Exchange
+
+
+class ExchangesView(generic.ListView):
+    template_name = 'exchange/exchanges.html'
+    context_object_name = 'exchanges'
+
+    def get_queryset(self):
+        return Exchange.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(ExchangesView, self).get_context_data(**kwargs)
+        request = self.request
+        user = request.user
+
+        # Check the user is logged in before searching
+        if user.is_authenticated:
+            # Methods that deal with user favourites and positions
+            user_bookmarks.get_user_favourites(user, context)
+            user_positions.get_open_positions(user, context)
+
+        return context
+
+
+class ExchangeStocksView(generic.ListView):
+    template_name = 'exchange/exchange_stocks.html'
+    context_object_name = 'stocks'
+    paginate_by = 5
+
+    def get_queryset(self):
+        pk = self.kwargs['pk']
+        return Stock.objects.filter(exchange_id=pk).order_by('ticker')
+
+    def get_context_data(self, **kwargs):
+        context = super(ExchangeStocksView, self).get_context_data(**kwargs)
+        request = self.request
+        user = request.user
+
+        pk = self.kwargs['pk']
+        exchange = Exchange.objects.get(id=pk)
+        context['exchange'] = exchange
+
+        # Check the user is logged in before searching
+        if user.is_authenticated:
+            # Methods that deal with user favourites and positions
+            user_bookmarks.check_exchange_stock(user, exchange, context)
+            user_bookmarks.get_user_favourites(user, context)
+            user_positions.get_open_positions(user, context)
+
+        return context
